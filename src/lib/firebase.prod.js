fc6f194a3ee34a9ef8a1c160d69655d4d8397697
Jan Dohinog <janreydohinog@gmail.com>
2021-07-11 12:24:18 +0800

    deploy

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 // 1) when seeding the database you'll have to uncomment this!
-//import { seedDatabase } from '../seed';
+import { seedDatabase } from '../seed';
 
 const config = {
   apiKey: "AIzaSyATJj4B1D_qxzuS-N_xUQXTNNosCHmqyIg",
